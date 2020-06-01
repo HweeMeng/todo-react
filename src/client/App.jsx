25ddf9ffd00a6e3b72437027652d25c6b5b36137
lekhweemeng <lek.hweemeng@live.com>
2020-06-02 00:16:35 +0800

    working copy that adds in value and displays

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -2,13 +2,52 @@ import React from 'react';
 import { hot } from 'react-hot-loader';
 
 class App extends React.Component {
-  render() {
-    return (
-      <div>
-        Welcome.
-      </div>
-    );
-  }
+    constructor(){
+      super()
+
+      this.state = {
+        holder:"",
+        todo : []
+      }
+    }
+    clickHandler(){
+        console.log('yay this button is working!')
+        var entry = this.state.holder
+        this.state.todo.push(entry);
+        console.log('todo array: ', this.state.todo)
+        var currentState = {
+            holder:"",
+            todo:this.state.todo
+        }
+        this.setState(currentState);
+    }
+
+    changeHandler(event){
+      console.log("change", event.target.value);
+      this.state.holder = event.target.value
+      console.log("this.state.holder", this.state.holder)
+    }
+
+    // }
+
+    render() {
+        console.log('rendering side, todo array: ', this.state.todo)
+        const todoMapper = this.state.todo.map((todo, index)=>{
+            return(
+                <div>
+                    {todo}
+                </div>
+                )
+        })
+        return (
+        <div>
+            Welcome.
+            <input onChange={(event)=>{this.changeHandler(event);}}/>
+            <button onClick={()=>{this.clickHandler()}}>click me!</button>
+            {todoMapper}
+        </div>
+        );
+    }
 }
 
 export default hot(module)(App);
